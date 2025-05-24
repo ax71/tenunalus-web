a3ecf36b12b6711f6a2ed2d54936af23fe3efd63
I kadek Buktiasa <146343510+ax71@users.noreply.github.com>
2025-05-24 12:13:20 +0800

    second commit tenunalus web

---
+++ app/api/inggest/route.js
@@ -0,0 +1,16 @@
+import { serve } from "inngest/next";
+import { inngest } from "@/config/inngest";
+import {
+  syncUserCreated,
+  syncUserDeletion,
+  syncUserUpdated,
+} from "@/config/inngest";
+
+// Create an API that serves zero functions
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [syncUserCreated, syncUserDeletion, syncUserUpdated],
+});
+
+
+//second commit 
