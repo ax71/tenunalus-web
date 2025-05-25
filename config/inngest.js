130a28da59c0671f4db34fea0bf492e6c19db5f4
I kadek Buktiasa <146343510+ax71@users.noreply.github.com>
2025-05-25 19:06:15 +0800

    fixx

--- config/inngest.js
+++ config/inngest.js
@@ -1,5 +1,5 @@
 import { Inngest } from "inngest";
-import { connectDB } from "./db";
+import { connectDB } from "../db";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "tenunalus" });
